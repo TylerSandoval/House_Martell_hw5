583444c66ca01f07a0a0807faa040bcbfc63ae0c
Tyler <tylersandoval@mail.weber.edu>
2017-10-28 20:47:41 -0600

    New GitHub project, working up to Part 1. Last GitHub project would not work/update.

---
+++ Roman.cpp
@@ -0,0 +1,81 @@
+//
+// Created by Tyler on 10/28/2017.
+//
+
+#include "Roman.h"
+
+unsigned int Roman::convertFromRoman(const string romanValue)
+{
+    unsigned int sum = 0;
+
+    for (int i = 0; i < romanValue.length(); i++)
+    {
+        switch(romanValue.at(i))
+        {
+            case 'M':
+                sum += 1000;
+                break;
+                
+            case 'D':
+                sum += 500;
+                break;
+
+            case 'C':
+                sum += 100;
+                break;
+
+            case 'L':
+                sum += 50;
+                break;
+
+            case 'X':
+                sum += 10;
+                break;
+
+            case 'V':
+                sum += 5;
+                break;
+
+            case 'I':
+                sum += 1;
+                break;
+
+            default:
+                cout << romanValue.at(i) << " is not a Roman Numeral!" << endl;
+                sum = 0;
+                break;
+
+        }//switch
+    }
+    return sum;
+}
+
+//This helps with testing, do not modify.
+bool checkTest(string testName, int whatItShouldBe, const Roman& obj)
+{
+    if (whatItShouldBe == obj.value)
+    {
+        cout << "Passed " << testName << endl;
+        return true;
+    }
+    else
+    {
+        cout << "****** Failed test " << testName << " ****** " << endl << "     Object contained: "<< obj.value << endl << "     Output should have contained: " << whatItShouldBe << endl;
+        return false;
+    }
+}
+
+/*
+void testOutput()
+{
+    Roman a("MDCLXVI");
+    string b = a.convertToRoman();
+    checkTest("testOutput #1", "MDCLXVI", b);
+
+    //This is really the value 7.  Your code should correctly read this in and convert it back to VII.
+    Roman c("IIIIIII");
+    b = c.convertToRoman();
+    checkTest("testOutput #2", "VII", b);
+
+}
+ */
---
+++ Roman.h
@@ -0,0 +1,45 @@
+//
+// Created by Tyler on 10/28/2017.
+//
+#include<iostream>
+#include<string>
+
+#ifndef HOUSE_MARTELL_HW5_ROMAN_H
+#define HOUSE_MARTELL_HW5_ROMAN_H
+
+using namespace std;
+
+/* This MUST be in a different source and header file */
+class Roman
+{
+private:
+    unsigned int value;  //The roman numeral itself isn't stored.  Instead, we just store the integer that corresponds to it.
+public:
+    Roman()
+    {
+        value = 0;      //The default constructor that allows us to make empty objects.
+    };
+    Roman(const string romanValue)
+    {
+        value = convertFromRoman(romanValue);        //The constructor which accepts a string and  converts it internally to an integer.  It actually just forwards it onto convertFromRoman()
+    };
+
+    unsigned int convertFromRoman(const string romanValue);  //The method that helps us take an roman numeral in a string and convert it into the integer.
+    //string convertToRoman() const;  //The method that helps us take an integer and turn it into a roman numeral
+
+    //Roman operator+(const Roman&) const;  //The left and right operands are Roman objects
+    //Roman operator+(const int) const; //The left operand is a Roman object, the right is an int number.
+    //void operator +=(const Roman&);  //The left and right operands are Roman objects, but the left operand can change.
+    //void operator +=(const int);  //The left operand is a Roman object, the right is an int number.  The left operand can change.
+    //Roman operator++();  //The prefix ++ operator
+
+    //These two are for testing purposes only.  They have been given to you.
+    friend bool checkTest(string, int , const Roman&);  //A testing function.  It is friended so it can access the class.
+    //friend void testOutput(); //Another test function that needs to access private class members.
+};
+
+
+
+
+
+#endif //HOUSE_MARTELL_HW5_ROMAN_H
